0493ef8df19b24b572db86a3e9eaed2cdeac2619
Danyal Jabarkhail <149615447+danyaljbarkhail@users.noreply.github.com>
2024-07-29 17:52:39 +0500

    Update app.js

--- fyp-portal-backend/app.js
+++ fyp-portal-backend/app.js
@@ -20,9 +20,9 @@ mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error('MongoDB connection error:', err));
 //Default Api
-// app.get("/", (req, res) > {
-// res.json("Hello");
-// });
+app.get("/", (req, res) > {
+res.json("Hello");
+});
 // Routes
 const authRoutes = require('./routes/auth');
 const supervisorRoutes = require('./routes/supervisors');
